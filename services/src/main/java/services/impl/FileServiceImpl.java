b1e67a6ecdcca6728c2235d8b54cc431f8ee199e
Nikolay Galanov <galanov.nick@gmail.com>
2016-07-07 16:29:07 +0300

    Implementing file management(deleting files): implemented.

--- services/src/main/java/services/impl/FileServiceImpl.java
+++ services/src/main/java/services/impl/FileServiceImpl.java
@@ -4,6 +4,8 @@ import com.google.common.base.Optional;
 import entity.File;
 import entity.tiny.FileId;
 import entity.tiny.UserId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import repository.FileRepository;
 import services.AuthenticationException;
 import services.FileService;
@@ -12,10 +14,14 @@ import services.UserAuthenticationService;
 import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  * Implementation of file management.
  */
 public class FileServiceImpl implements FileService {
+
+    private final Logger log = LoggerFactory.getLogger(FileServiceImpl.class);
 
     private final FileRepository fileRepository;
 
@@ -30,6 +36,15 @@ public class FileServiceImpl implements FileService {
     public void add(AuthenticationToken token,
                     File file, FileInputStream inputStream) throws AuthenticationException {
 
+        checkNotNull(token, "Authentication token cannot be null");
+        checkNotNull(file, "File cannot be null");
+        checkNotNull(inputStream, "File input stream cannot be null");
+
+        if (log.isDebugEnabled()) {
+            log.debug("Adding file (filename = \"" + file.getName()
+                    + "\") by user with id = \"" + file.getOwnerId() + "\"");
+        }
+
         if (authenticationService.checkAuthentication(token, file.getOwnerId())) {
             fileRepository.add(file, inputStream);
         } else {
@@ -40,6 +55,15 @@ public class FileServiceImpl implements FileService {
     @Override
     public ByteArrayInputStream getFileContent(AuthenticationToken token, FileId fileId, UserId userId)
             throws AuthenticationException {
+
+        checkNotNull(token, "Authentication token cannot be null");
+        checkNotNull(fileId, "File id cannot be null");
+        checkNotNull(userId, "User id cannot be null");
+
+        if (log.isDebugEnabled()) {
+            log.debug("Getting file content(file id = \""
+                    + fileId.get() + "\" by user with id = \"" + userId + "\"");
+        }
 
         if (authenticationService.checkAuthentication(token, userId)) {
 
@@ -58,6 +82,15 @@ public class FileServiceImpl implements FileService {
     @Override
     public File getFileMeta(AuthenticationToken token, FileId fileId, UserId userId) throws AuthenticationException {
 
+        checkNotNull(token, "Authentication token cannot be null");
+        checkNotNull(fileId, "File id cannot be null");
+        checkNotNull(userId, "User id cannot be null");
+
+        if (log.isDebugEnabled()) {
+            log.debug("Getting file meta(file id = \""
+                    + fileId.get() + "\" by user with id = \"" + userId + "\"");
+        }
+
         if (authenticationService.checkAuthentication(token, userId)) {
 
             Optional<File> fileContent = fileRepository.getFileMeta(fileId);
@@ -75,5 +108,24 @@ public class FileServiceImpl implements FileService {
     @Override
     public void deleteFile(AuthenticationToken token, FileId fileId, UserId userId) {
 
+        if (log.isDebugEnabled()) {
+            log.debug("Trying remove file with id = \"" + fileId + "\" by user with id = \"" + userId + "\".");
+        }
+
+        Optional<File> fileMeta = fileRepository.getFileMeta(fileId);
+
+        if (fileMeta.isPresent()) {
+            if (!fileMeta.get().getOwnerId().equals(userId)) {
+                throw new IllegalStateException("File can be removed only by owner.");
+            }
+
+            fileRepository.removeFile(fileId);
+
+            if (log.isDebugEnabled()) {
+                log.debug("File (fileId = \"" + fileMeta.get().getId().get() + "\" has been removed.");
+            }
+        } else {
+            throw new IllegalArgumentException("Invalid file id.");
+        }
     }
 }
