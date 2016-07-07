196d408e846e3756383f20cf12e91dabcfe962c8
Nikolay Galanov <galanov.nick@gmail.com>
2016-07-07 15:21:42 +0300

    Implementing file management(downloading file): public api added.

--- services/src/main/java/services/FileService.java
+++ services/src/main/java/services/FileService.java
@@ -1,6 +1,11 @@
 package services;
 
 import entity.File;
+import entity.tiny.FileId;
+import services.impl.AuthenticationToken;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 
 /**
  * Interface for file management.
@@ -11,5 +16,10 @@ public interface FileService {
      * Add file with specified user as owner.
      * @param file - file to be added.
      */
-    void add(File file);
+    void add(AuthenticationToken token, File file, FileInputStream inputStream)
+            throws AuthenticationException;
+
+    ByteArrayInputStream getFileContent(AuthenticationToken token, FileId fileId);
+
+    File getFileMeta(AuthenticationToken token, FileId fileId);
 }
